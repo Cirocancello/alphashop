f8884b83236d112dae4c5dc7ec1468c433409d80
Ciro Cancello <cirocancello75@gmail.com>
2023-08-08 14:27:18 +0200

    aggiunta della cartella pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { ErrorComponent } from './error/error.component';
-import { ArticoliComponent } from './articoli/articoli.component';
-import { LogoutComponent } from './logout/logout.component';
-import { RouteGuardService } from 'src/service/route-guard-service.service';
+
+import { ArticoliComponent } from './pages/articoli/articoli.component';
+import { ErrorComponent } from './pages/error/error.component';
+import { LoginComponent } from './pages/login/login.component';
+import { LogoutComponent } from './pages/logout/logout.component';
+import { NgModule } from '@angular/core';
+import { RouteGuardService } from '../services/route-guard.service';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 
 const routes: Routes = [
   {path:'', component: LoginComponent},
   {path:'login', component: LoginComponent},
-  {path:'welcome/:userid', component: WelcomeComponent, canActivate: [RouteGuardService]},  
-  {path:'logout', component: LogoutComponent},
-  {path:'articoli', component: ArticoliComponent, canActivate: [RouteGuardService]},  
-
-  {path:'**', component: ErrorComponent}  
+  {path:'welcome/:userid', component: WelcomeComponent, canActivate:[RouteGuardService]},
+  {path:'articoli', component : ArticoliComponent, canActivate:[RouteGuardService]},
+  {path:'logout', component : LogoutComponent},
+  {path:'**', component: ErrorComponent},
 ];
 
 @NgModule({
